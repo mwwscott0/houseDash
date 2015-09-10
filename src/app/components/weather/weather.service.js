c48e88e1224d662947adeb6ced3b70d3db0f24e0
Ryan <Rhinomcd@gmail.com>
2015-09-09 21:45:08 -0700

    set up .get requestfor openweather api

--- src/app/components/weather/weather.service.js
+++ src/app/components/weather/weather.service.js
@@ -3,6 +3,32 @@
 
 angular
 .module('houseDash')
-.factory('weather', weather)
+.factory('weather', weather);
 
+  /** @ngInject */
+  function weather($http) {
+    var apiHost = 'https://api.openweathermap.org/data/2.5/weather?zip=94040,us';
+
+    var service = {
+      apiHost: apiHost,
+      getWeather: getWeather
+    };
+
+    return service;
+
+    function getWeather {
+
+      return $http.get(apiHost)
+        .then(getWeatherComplete)
+        .catch(getWeatherFailed);
+
+      function getWeatherComplete(response) {
+        return response.data;
+      }
+
+      function getWeatherFailed(error) {
+        $log.error('XHR Failed for getWeather.\n' + angular.toJson(error.data, true));
+      }
+    }
+  }
 })();
